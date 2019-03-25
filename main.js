8a5ac8d13787e150c7afdd473b3e3b6070c86bf4
Marco Loyo <mloyo@intersysconsulting.com>
2019-03-25 15:56:55 -0600

    specify function as const

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Octokit = require('@octokit/rest')
 const octokit = new Octokit ()
 
-getRepos = async (user) => {
+const getRepos = async (user) => {
     const {data} = await octokit.repos.listForUser({username:user});
     console.log(data);
 }
