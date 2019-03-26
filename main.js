5c90dbc96a7c8e2c32ad4e175d930f302dcda46a
L0y3r <marcoantonioloyom@hotmail.com>
2019-03-26 13:02:03 -0600

    add .env support files and improve manage variables

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const Octokit = require('@octokit/rest')
-const octokit = new Octokit ()
+require('dotenv').config();
+
+const Octokit = require('@octokit/rest');
+const octokit = new Octokit ({
+    auth: process.env.GITHUB_TOKEN
+});
 
 const getRepos = async (user) => {
     let reposAndTheirIssues = [];
     const {data} = await octokit.repos.listForUser({username:user});
-    dataLength = data.length;
+    let dataLength = data.length;
     while (dataLength) {
         dataLength--;
-        repoName = data[dataLength].name;
+        let repoName = data[dataLength].name;
         const issues = await octokit.issues.listForRepo({
             owner:user,
             repo:repoName
         });
-        reposAndTheirIssues.push({repoName: issues.data});
+        reposAndTheirIssues.push({repo: repoName, issues: issues.data});
     }
     console.log(reposAndTheirIssues);
 }
