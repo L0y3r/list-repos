e2742b6c99606e8696f17713cebe2a6cbabcb0f1
L0y3r <marcoantonioloyom@hotmail.com>
2019-03-26 17:55:51 -0600

    improve time response with Promise all

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 require('dotenv').config();
 
 const Octokit = require('@octokit/rest');
-const octokit = new Octokit ({
+const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 
 const getRepos = async (user) => {
-    let reposAndTheirIssues = [];
-    const {data} = await octokit.repos.listForUser({username:user});
-    let dataLength = data.length;
-    while (dataLength) {
-        dataLength--;
-        let repoName = data[dataLength].name;
-        const issues = await octokit.issues.listForRepo({
-            owner:user,
-            repo:repoName
-        });
-        reposAndTheirIssues.push({repo: repoName, issues: issues.data});
+
+    let reposPromises = [];
+    const { data } = await octokit.repos.listForUser({ username: user }),
+        dataLength = data.length;
+
+    for (let index = 0; index < dataLength; index++) {
+        const repoName = data[index].name;
+
+        reposPromises.push(octokit.issues.listForRepo({
+            owner: user,
+            repo: repoName
+        }));
     }
-    console.log(reposAndTheirIssues);
+
+    Promise.all(reposPromises)
+        .then(responses => {
+
+            let reposAndTheirIssues = [];
+            const responsesLength = responses.length;
+
+            for (let index = 0; index < responsesLength; index++) {
+                
+                const repoName = data[index].name,
+                    repoIssues = responses[index].data;
+                
+                reposAndTheirIssues.push({
+                    repo: repoName,
+                    issues: repoIssues
+                });
+            }
+
+            console.log(reposAndTheirIssues);
+        })
+        .catch(reason => {
+            console.error(reason);
+        });
+
 }
 getRepos('IntersysConsulting');
