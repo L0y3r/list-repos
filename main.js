d532d940fefba0d3b1f3ea337dc784b87c87a335
L0y3r <marcoantonioloyom@hotmail.com>
2019-03-26 12:16:05 -0600

    map repos with their issues

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const Octokit = require('@octokit/rest')
 const octokit = new Octokit ()
 
 const getRepos = async (user) => {
+    let reposAndTheirIssues = [];
     const {data} = await octokit.repos.listForUser({username:user});
     dataLength = data.length;
     while (dataLength) {
         dataLength--;
         repoName = data[dataLength].name;
-        const result = await octokit.issues.listForRepo({
+        const issues = await octokit.issues.listForRepo({
             owner:user,
             repo:repoName
         });
-        console.log(repoName, result.data);
+        reposAndTheirIssues.push({repoName: issues.data});
     }
+    console.log(reposAndTheirIssues);
 }
 getRepos('IntersysConsulting');
